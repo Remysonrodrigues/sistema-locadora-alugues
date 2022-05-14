910517ea528813657dd7dc959300e15f818df9d9
Remyson <remysonrodrigues@gmail.com>
2022-05-14 10:38:40 -0300

    Criando uma suite de teste

---
+++ src/test/java/suites/SuiteExecucao.java
@@ -0,0 +1,25 @@
+package suites;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import services.CalculadoraTest;
+import services.CalculoValorLocacaoTest;
+import services.LocacaoServiceTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    CalculadoraTest.class,
+    CalculoValorLocacaoTest.class,
+    LocacaoServiceTest.class
+})
+public class SuiteExecucao { // Executa uma suite de testes
+
+    @BeforeClass
+    public static void before() {}
+
+    @AfterClass
+    public static void after() {}
+
+}
