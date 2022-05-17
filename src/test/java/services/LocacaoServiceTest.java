d4b2e9ea6807bfa4a28aca68352bb105fa2938ba
Remyson <remysonrodrigues@gmail.com>
2022-05-17 18:06:49 -0300

    add Mockito

--- src/test/java/services/LocacaoServiceTest.java
+++ src/test/java/services/LocacaoServiceTest.java
@@ -14,6 +14,7 @@ import builders.UsuarioBuilder;
 import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -37,7 +38,7 @@ public class LocacaoServiceTest {
     @Before
     public void setup() { // Executa antes de cada teste
         service = new LocacaoService();
-        LocacaoDAO dao = new LocacaoDAOFake();
+        LocacaoDAO dao = Mockito.mock(LocacaoDAO.class);
         service.setLocacaoDAO(dao);
     }
 
